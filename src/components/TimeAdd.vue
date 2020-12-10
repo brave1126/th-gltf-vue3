6e7f6e6d260e3e5c6b3fdb02d6d62b39d3fba0d5
baix <baix@baix.com>
2020-12-10 12:24:53 +0800

    vue3 组合api学

---
+++ src/components/TimeAdd.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ time }}/{{ dbTime }}
+    <a-button type="primary" @click="timeAdd">
+      Primary
+    </a-button>
+  </div>
+</template>
+
+<script>
+// import { ref, watch, onMounted, computed } from "vue";
+import times from "./time";
+export default {
+  setup() {
+    let { time, dbTime, timeAdd } = times();
+    return { time, dbTime, timeAdd };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <time-add></time-add>
+</template>
+
+<script>
+import TimeAdd from "../components/TimeAdd.vue";
+export default {
+  components: { TimeAdd },
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
